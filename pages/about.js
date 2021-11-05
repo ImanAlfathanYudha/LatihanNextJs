1cb34cc29f1a25676f019ef5e87b33e4aa1497de
Yudha <iman.yudhanto@amartha.com>
2021-11-05 10:34:10 +0700

    main. adding ninjas files.

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About= () => {
+    return (
+        <div>
+          <h1>About</h1>  
+          <p>Proyek preketek: manusia tulang lunak.</p>
+          <p>Proyek preketek: manusia tulang lunak.</p>
+        </div>
+    )
+}
+
+export default About;
