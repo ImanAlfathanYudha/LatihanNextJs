9562e815c17817a2ecfde8cd74f50a9d191d5c79
Yudha <iman.yudhanto@amartha.com>
2021-11-09 20:38:43 +0700

    main. adding method for getStaticProps.

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,13 +1,47 @@
 import Link from 'next/link'
-const Ninjas= () => {
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+const Ninjas= ({ninjas}) => {
+    console.log(ninjas)
     return (
         <div>
-          <h1>All Ninjas</h1>  
+          <h1>All Ninjas</h1>
           <Link href="/ninjas/test">
             <a>Test tist</a>
           </Link>
+            {ninjas.map(ninja=>(
+              <div key={ninja.id}>
+              <a className={styles.single}>
+                <h3>{ninja.name}</h3>
+              </a>
+              </div>
+            ))}
         </div>
     )
 }
+
+// const Ninjas = ({ ninjas }) => {
+//   console.log(ninjas)
+//   return (
+//     <div>
+//       <h1>All Ninjas</h1>
+//       {ninjas.map(ninja => (
+//         <div key={ninja.id}>
+//           <a className={styles.single}>
+//             <h3>{ ninja.name }</h3>
+//           </a>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 export default Ninjas;
--- pages/ninjas/test.js
+++ pages/ninjas/test.js
@@ -1,14 +1,29 @@
-const Test= () => {
+import styles from '../../styles/Ninjas.module.css'
+export const getStaticProps = async () => {
+  //entar pas dipuka di laptop sebelah buat tactical notes.
+  // const res = await fetch('https://jsonplaceholder.typicode.com/users/1/albums');
+  // const data = await res.json();
+
+  // return {
+  //   props: { listAlbum: data }
+  // }
+}
+
+const Test= ({listAlbum}) => {
+  console.log("tes listAlbum ",listAlbum)
     return (
         <div>
           <h1><b>Test</b></h1>  
-          <h1><b>Test</b></h1> 
-          <h1><b>Test</b></h1> 
-          <h1><b>Test</b></h1> 
-          <h1><b>Test</b></h1> 
-          <h1><b>Tis</b></h1> 
+          {listAlbum.map(album=>(
+              <div key={album.id}>
+              <a className={styles.single}>
+                <h3>{album.title}</h3>
+              </a>
+              </div>
+            ))}
         </div>
     )
 }
 
 export default Test;
+// https://jsonplaceholder.typicode.com/users/1/albums
