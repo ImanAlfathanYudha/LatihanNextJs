199a4b71b867c87f512593037be57c4f6cb109e3
alfathaniman <alfathan.iman@gmail.com>
2021-12-11 17:57:51 +0700

    main. improving ninja page.

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import styles from '../../styles/Ninjas.module.css'
+import Ninja from '../../comps/ninja';
+import UserPost from './[id]/post';
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -9,21 +11,22 @@ export const getStaticProps = async () => {
     props: { ninjas: data }
   }
 }
+
 const Ninjas= ({ninjas}) => {
+  console.log("Tes ninjas apaan ", ninjas)
     return (
         <div>
           <h1>All Ninjas</h1>
           <Link href="/ninjas/test">
             <a>Test tist</a>
           </Link>
-            {ninjas.map(ninja=>(
-              <div className={styles.single}>
-                <Link href={'/ninjas/' + ninja.id} key={ninja.id}><a><h3>{ninja.name}</h3></a></Link>
-                <Link href={'/ninjas/' + ninja.id+'/post'} key={ninja.id}>Posts</Link>
-                <br></br>
-                <Link href={'/ninjas/' + ninja.id+'/post'} key={ninja.id} replace><a><b>BACK</b></a></Link>
-              </div>
-            ))}
+            {ninjas.map((ninja)=>{
+              return(
+                <div>
+                  <Ninja ninja={ninja}></Ninja>
+                </div>
+              )
+              })}
         </div>
     )
 }
