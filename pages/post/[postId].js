88ef9dec1b1ba7a0ae4e5a4f5e572a3793a4c1c6
alfathaniman <alfathan.iman@gmail.com>
2022-01-16 16:58:39 +0700

    main. adding method if fallback is true.

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,7 +1,16 @@
+import {useRouter} from 'next/router' 
+
 export async function getStaticProps(context) {
     const id = context.params.postId;
     const res = await fetch('https://jsonplaceholder.typicode.com/posts/'+id);
+    console.log("Tes getStaticProps klo fallback true", id)
     const data = await res.json();
+    if (!data.id) {
+        return {
+            notFound:true
+        }
+    }
+    console.log("Tes getStaticProps klo fallback true", id)
     return {
         props: { postDetail: data}
     }
@@ -10,17 +19,35 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await res.json();
+    data
     const paths = data.map(post=>{
         return {
             params: {postId:post.id.toString()}
         }
     })
     return {
-        paths: paths, fallback: false
+        paths: paths, fallback: true
     }
 }
 
+// export async function getStaticPaths() {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await res.json();
+//     const paths = data.map(post=>{
+//         return {
+//             params: {postId:post.id.toString()}
+//         }
+//     })
+//     return {
+//         paths: paths, fallback: false
+//     }
+// }
+
 const postDetail= ({postDetail}) => {
+    const router = useRouter()
+    if (router.isFallback) {
+        return <h1>Loading, om...</h1>
+    }
     return (
         <div>
         <h1>Post Pages Detail &#x2611;</h1>
