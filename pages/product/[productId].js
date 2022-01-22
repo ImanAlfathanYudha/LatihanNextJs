bc92c1c9286e0ca570c19c5b25025d34d461daea
Yudha <iman.yudhanto@amartha.com>
2022-01-22 19:40:15 +0700

    main. adding product detail method and db.json

---
+++ pages/product/[productId].js
@@ -0,0 +1,69 @@
+import {useRouter} from 'next/router' 
+
+export async function getStaticProps(context) {
+    const id = context.params.productId;
+    const res = await fetch('http://localhost:4000/product/'+id);
+    console.log("Tes getStaticProps klo fallback true", id)
+    const data = await res.json();
+    if (!data.id) {
+        return {
+            notFound:true
+        }
+    }
+    console.log("Tes getStaticProps klo fallback true", id)
+    return {
+        props: { productDetail: data}
+    }
+}
+
+//fallback kalo true
+export async function getStaticPaths() {
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const data = await res.json();
+    // data
+    // const paths = data.map(post=>{
+    //     return {
+    //         params: {postId:post.id.toString()}
+    //     }
+    // })
+    return {
+        paths: [{
+            params:{
+                productId:'1'
+            }
+        }], fallback: true
+    }
+}
+
+//fallback kalo false
+// export async function getStaticPaths() {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     const data = await res.json();
+//     const paths = data.map(post=>{
+//         return {
+//             params: {postId:post.id.toString()}
+//         }
+//     })
+//     return {
+//         paths: paths, fallback: false
+//     }
+// }
+
+const productDetail= ({productDetail}) => {
+    const router = useRouter()
+    if (router.isFallback) {
+        return <h1>Loading product...</h1>
+    }
+    return (
+        <div>
+        <h1>Product Pages Detail &#x2611;</h1>
+              <div>
+              <h3>{productDetail.id} {productDetail.title}</h3>
+              <h4>Rp{productDetail.price}</h4>
+                    <p> {productDetail.description}</p>
+              </div>
+      </div>
+    )
+}
+
+export default productDetail
