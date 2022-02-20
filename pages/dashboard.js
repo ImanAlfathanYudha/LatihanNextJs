e701d43ace76412cf6f668906ded29789440fb61
Yudha <iman.yudhanto@amartha.com>
2022-02-20 20:35:37 +0700

    main. adding dashboard page with client side data fetching.

---
+++ pages/dashboard.js
@@ -0,0 +1,59 @@
+import {useState, useEffect} from 'react'
+function Dashboard() {
+    const[isLoading, setIsLoading] = useState(true)
+    const[dashboardData, setDashboardData] = useState(0)
+    const[userData, setUserData] = useState(0)
+    useEffect(()=>{
+        async function fetchDashboardData() {
+            const response = await fetch('http://localhost:4000/dashboard');
+            const data = await response.json()
+            setDashboardData(data)
+            setIsLoading(false)
+        }
+
+        async function fetchUserData() {
+            const response = await fetch('http://localhost:4000/profile');
+            const data = await response.json()
+            setUserData(data)
+            setIsLoading(false)
+        }
+
+        fetchDashboardData();
+        fetchUserData();
+    },[])
+     console.log("Tes dashboardData final ", dashboardData)
+    // useEffect(()=>{
+    //     async function fetchUserData() {
+    //         const response = await fetch('http://localhost:4000/profile');
+    //         const data = await response.json()
+    //         setUserData(data)
+    //         setIsLoading(false)
+    //     }
+    //     fetchUserData()
+    //     console.log("Tes userData final ", userData)
+    // },[])
+    console.log("Tes userData final ", userData)
+
+    if (isLoading){
+        return <h1>Loading, om...</h1>
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>User Profile: </h1>
+                <div>
+                <h3>{userData.name} </h3>
+                        <p> {userData.username}</p>
+                </div>
+            </div>
+            <h2>Dashboard</h2>
+            <h3>Post - {dashboardData.posts}</h3>
+            <h3>Likes - {dashboardData.likes}</h3>
+            <h3>Followers - {dashboardData.followers}</h3>
+            <h3>Following - {dashboardData.followings}</h3>
+        </div>
+    )
+}
+
+export default Dashboard;
