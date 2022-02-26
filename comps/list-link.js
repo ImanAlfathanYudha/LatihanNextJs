66689d1cca129c31a1a14974b3088fd5b7c70b1a
Yudha <iman.yudhanto@amartha.com>
2022-02-26 22:02:25 +0700

    main. adding event page with pre-rendering and client side data fetiching.

--- comps/list-link.js
+++ comps/list-link.js
@@ -7,6 +7,11 @@ const ListLink= ({data, type}) => {
         linkHTML = <Link href={`product/${data.id}`} passHref={true}><h3>{data.id} {data.title}</h3></Link>
     } else if (type=="news"){
         linkHTML = <Link href={`news/${data.id}`} passHref={true}><h3>{data.id} {data.title} | {data.category}</h3></Link>
+    } else if (type=="event"){
+        linkHTML = <div><h2>
+            {data.id} {data.title} {data.date} | {data.category}
+        </h2>
+        <p>{data.description}</p><hr/></div>
     }
     return (
         <div key={data.id} className={styles.single}>
---
+++ pages/event.js
@@ -0,0 +1,52 @@
+import ListLink from '../comps/list-link'
+import {useState} from 'react'
+import { useRouter } from 'next/router'
+
+export async function getServerSideProps(context) {
+    const {query} = context
+    const {category} = query
+    const categoryString= category?'category=sports':''
+    const response = await fetch('http://localhost:4000/events?'+categoryString);
+    const data = await response.json()
+    return {
+        props: { eventList: data}
+    }
+}
+
+function EventList({eventList}) {
+    const router = useRouter()
+    const[events, setEventData] = useState(eventList)
+
+    const resetEvent = async()=>{
+        const response = await fetch('http://localhost:4000/events');
+        const data = await response.json()
+        setEventData(data)
+        router.push('event', undefined,{shallow:true})
+        // router.reload(window.location.pathname)
+    }
+
+    const fetchSportEvent = async()=>{
+        const response = await fetch('http://localhost:4000/events?category=sports');
+        const data = await response.json()
+        console.log("Tes fetchSportEvent ", data)
+        setEventData(data)
+        router.push('event?category=sports', undefined,{shallow:true})
+    }
+    return (
+        <>
+        <button onClick={fetchSportEvent}>Sport Event</button>
+        <button onClick={resetEvent}>Reset</button>
+    <h1>List of Event</h1>
+    {events.map((event)=>{
+            return (
+                <div key={event.id}>
+                {/* <Link href={`news/${article.id}`} passHref={true}><h2>{article.id} {article.title} | {article.category}</h2></Link> */}
+                <ListLink data={event} type="event"></ListLink>
+                </div>
+            )
+        })}       
+    </>
+    )
+}
+
+export default EventList
