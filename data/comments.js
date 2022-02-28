ee1a6012d4b4f1c98cdd5d1c031f6c939d038f8f
alfathaniman <alfathan.iman@gmail.com>
2022-02-28 23:10:39 +0700

    main. adding data folder as dummy model

---
+++ data/comments.js
@@ -0,0 +1,15 @@
+export const comments = [
+    {
+        id:1,
+        text:"This is comments."
+    },
+    {
+        id:2,
+        text:"Pertamax, gan!"
+    },
+    {
+        id:3,
+        text:"Dijual rumah dengan harga 700 jta rupiah di daerah beji. Hubungi 081308130813 jika minat."
+    }
+
+]
