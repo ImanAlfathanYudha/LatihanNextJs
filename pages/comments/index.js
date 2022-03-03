775752863dddefb06f41f94d767c3ea009b56e0c
alfathaniman <alfathan.iman@gmail.com>
2022-03-03 23:32:01 +0700

    main. adding insert comment function.

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 function CommentsPage() {
     const [comments, setComment] = useState([])
+    const [inputComment, setCommentInput] = useState('')
     var htmlButtonLoadComment = ""
     const fetchComments = async()=>{
         const response = await fetch('/api/comments')
@@ -9,12 +10,32 @@ function CommentsPage() {
         setComment(data)
         console.log("Tes fetchComments ", data)
     }
+
+    const submitComment = async()=>{
+        console.log("Tes submitComment invoked")
+        const response = await fetch('/api/comments',{
+            method:'POST',
+            body: JSON.stringify({inputComment}),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        window.alert("Comment berhasil diinput")
+        fetchComments()
+        const data = await response.json()
+        console.log("Tes submitComment data ", data)
+    }
+
     console.log("tes barangkeles ada comments ", comments)
     if (comments.length==0){
-        htmlButtonLoadComment = <button onClick={fetchComments}>Load Comment</button>
+        htmlButtonLoadComment = <div><button onClick={fetchComments}>Load Comment</button></div>
     }
     return (
         <>
+        <div>
+        <input type='text' value={inputComment} onChange={e=>setCommentInput(e.target.value)}></input>
+        <button onClick={submitComment}>Submit Comment</button>
+        </div>
         {htmlButtonLoadComment}
         {
             comments.map((comment) => {
