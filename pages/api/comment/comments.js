934ce4ff782a5ea0fded97ad1790e4886824424e
alfathaniman <alfathan.iman@gmail.com>
2022-04-03 00:30:35 +0700

    main. adding delete comments function.

--- pages/api/comment/comments.js
+++ pages/api/comment/comments.js
@@ -14,6 +14,7 @@ export default function handler(req, res) {
         }
         console.log("Tes newComment ",newComment)
         comments.push(newComment)
+        console.log("Tes comments final ",comments)
         res.status(200).json
     }
 }
