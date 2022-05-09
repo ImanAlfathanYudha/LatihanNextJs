3710cfe597221ba961066b779728440828e50e39
alfathaniman <alfathan.iman@gmail.com>
2022-05-09 23:48:04 +0700

    main. fixing comments in all-catcher api.

--- pages/api/[[...params]].js
+++ pages/api/[[...params]].js
@@ -2,12 +2,11 @@
 export default function handler(req, res) {
     const params = req.query.params
     if (params) {
-        console.log("Tes parents api params ", params)
         res.status(200).json({
             params
         })
     }
-    console.log("Tes parents api params ", params)
+    console.log("Tes parents api params empty ", params)
     res.status(200).json({
         test:"Home API"
     })
