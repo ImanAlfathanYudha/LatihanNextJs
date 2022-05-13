7e97bf913ee449c56b0d6ef79f06c140a90c04d1
alfathaniman <alfathan.iman@gmail.com>
2022-05-14 00:35:39 +0700

    main. adding new line on new contact page.

--- pages/new/contact.js
+++ pages/new/contact.js
@@ -1,7 +1,10 @@
 import styles from '../../styles/Contact.module.css'
 
 function Contact() {
-    return ( <div className={styles.highlight}><h2>Contact Page </h2></div>)
+    return ( <div className={styles.highlight}>
+        <h2>Contact Page </h2>
+        <a href="https://www.instagram.com/ravenerza">Instagram</a>
+        </div>)
 }
 
 export default Contact   
