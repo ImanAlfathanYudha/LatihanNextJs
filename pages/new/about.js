f36661e9cb1f59461171c5478afba9d344d17fd2
Yudha <iman.yudhanto@amartha.com>
2022-05-13 20:10:25 +0700

    main. adding new style for some pages and new pages

---
+++ pages/new/about.js
@@ -0,0 +1,10 @@
+import styles from '../../styles/About.module.css'
+
+function About() {
+    return (<div className={styles.highlight}><h2>About Page </h2>
+        <button className='btn btn-primary'>Primary</button>
+        </div>
+    )
+}
+
+export default About  
---
+++ pages/new/contact.js
@@ -0,0 +1,7 @@
+import styles from '../../styles/Contact.module.css'
+
+function Contact() {
+    return ( <div className={styles.highlight}><h2>Contact Page </h2></div>)
+}
+
+export default Contact   
