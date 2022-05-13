cf134f69c6e11940bb735fba2225d2adfce31233
alfathaniman <alfathan.iman@gmail.com>
2022-05-13 23:58:37 +0700

    main. adding new line.

--- pages/new/about.js
+++ pages/new/about.js
@@ -2,6 +2,7 @@ import styles from '../../styles/About.module.css'
 
 function About() {
     return (<div className={styles.highlight}><h2>About Page </h2>
+        <p>This next js app is for learning purpose.</p>
         <button className='btn btn-primary'>Primary</button>
         </div>
     )
