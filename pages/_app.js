8014b72e1761bd6b58bf8a10c91e3981148237f9
alfathaniman <alfathan.iman@gmail.com>
2022-05-18 00:43:28 +0700

    main. adding scss styling.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import Layout from '../comps/layout'
 import '../styles/globals.css'
+import '../styles/About.Module.scss'
+import '../styles/Contact.Module.scss'
 // import 'bootstrap/dist/css/bootstrap.min.css '
 
 function MyApp({ Component, pageProps }) {
