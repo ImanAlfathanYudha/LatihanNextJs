778ca2886f482fe3feacef05df3f6cc6d93415ac
alfathaniman <alfathan.iman@gmail.com>
2022-05-20 00:41:24 +0700

    main. adding a new content.

--- pages/css-in-js.js
+++ pages/css-in-js.js
@@ -1,22 +1,25 @@
 import styled from 'styled-components'
 
 const Title = styled.h1`
-    font-size: 50px;
+    font-size: ${({theme})=>theme.fontSize.big};
     color:${({theme})=>theme.colors.primary};`
 
-// ntar dibuka
 const NormalText = styled.p`
     font-size: 20px;
     color: #eaaaf2;` 
+
+const MiniText = styled.p`
+    font-size: ${({theme})=>theme.fontSize.small};
+`
 
 function CSSJS() {
     return(
         <div>
             <Title>Styled-Components</Title>
-            <NormalText>CSS kadang muncul kadang engga hehe</NormalText>
+            <NormalText>CSS kadang muncul kadang engga</NormalText>
+            <MiniText>Hehe</MiniText>
         </div>
-        
-        // <h2 style={{olor:'red'}}>CSS IN JS, HELLO WORLD!</h2>
+        // <h2 style={{color:'red'}}>CSS IN JS, HELLO WORLD!</h2>
     )
 }
 
