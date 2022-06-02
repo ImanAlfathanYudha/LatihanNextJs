4411622a8106bd00d62736b9c1e310dcf310d608
alfathaniman <alfathan.iman@gmail.com>
2022-06-02 23:58:05 +0700

    main. removing unused comments and logs.

--- pages/news/detail/[newsId].js
+++ pages/news/detail/[newsId].js
@@ -5,7 +5,6 @@ export async function getStaticProps(context) {
     console.log("Tes regenerating news detail id ", id)
     const res = await fetch('http://localhost:4000/news/'+id);
     const data = await res.json();
-    console.log("Tes data ", data)
     if (!data.id) {
         return {
             notFound:true
@@ -28,26 +27,11 @@ export async function getStaticPaths() {
     }
 }
 
-//fallback kalo false
-// export async function getStaticPaths() {
-//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-//     const data = await res.json();
-//     const paths = data.map(post=>{
-//         return {
-//             params: {postId:post.id.toString()}
-//         }
-//     })
-//     return {
-//         paths: paths, fallback: false
-//     }
-// }
-
 const NewsArticleDetail= ({newsArticleDetail}) => {
     const router = useRouter()
     if (router.isFallback) {
         return <h1>Loading product...</h1>
     }
-    console.log("Tes newsArticleDetail hehe ",newsArticleDetail)
     return (
         <div>
         <h1>Article Pages Detail &#x2611;</h1>
